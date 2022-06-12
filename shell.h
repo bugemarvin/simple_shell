76499706e0167b698c7e817e23eac301d5b2c094
Marvin Kurland <bugemarvin@outlook.com>
2022-06-12 13:37:21 +0300

    first commit

--- shell.h
+++ shell.h
@@ -5,8 +5,22 @@
  * Authors: Marvin Kurland.
  *		Sospeter Nyaga.
  *
+ * 2022 (c) Alx project.
  * Prototypes for `C` Simple Shell.
  */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+#include <signal.h>
+#include <pwd.h>
+#include <glob.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <string.h>
 
 int _putchar(char c);
 
